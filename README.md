f29f175dfb3254512584ca4974983395402d80e5
carlosptt <carlosptt@gmail.com>
2019-03-28 22:26:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-kvsjre
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-kvsjre)
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular 6';
+
+
+  public myfunction(message : string){
+      alert(message);
+  }
+}
